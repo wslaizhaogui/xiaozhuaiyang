b718ecca7f48443ff5bcb9b467bc26bc77aa3ad8
893192367@qq.com <893192367@qq.com>
2021-04-22 23:30:31 +0800

    aaa

---
+++ src/main/java/com/example/xiaozhuaiyang/XiaozhuaiyangApplication.java
@@ -0,0 +1,13 @@
+package com.example.xiaozhuaiyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaozhuaiyangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaozhuaiyangApplication.class, args);
+    }
+
+}
