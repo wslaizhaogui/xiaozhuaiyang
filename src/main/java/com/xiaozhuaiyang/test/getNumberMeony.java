87dd0c7b0373bd658d50c9c004e8f3555a5e14cb
893192367@qq.com <893192367@qq.com>
2021-06-07 11:49:04 +0800

    jpa用户增删改查

---
+++ src/main/java/com/xiaozhuaiyang/test/getNumberMeony.java
@@ -0,0 +1,45 @@
+package com.xiaozhuaiyang.test;
+
+import java.util.*;
+
+/**
+ * @Author: zglai
+ * Date:23:05 2021/6/2
+ * @Description:<描述>
+ */
+public class getNumberMeony {
+    public static void main(String[] args) {
+        Random random = new Random();
+        //21060
+        List<Integer> excludeNum5 = Arrays.asList(1,4,6,15,19,20,23,26,32,34,33,35);
+        List<Integer> excludeNum2 = Arrays.asList(1,3,4,5,7,9,11,12);
+        for(int i=0;i<10;i++){
+            List<Integer> num5 = new ArrayList<>();
+            List<Integer> num2 = new ArrayList<>();
+            //选5个
+            while (true){
+                if(num5.size() == 5){
+                    break;
+                }
+                int n = random.nextInt(35)+1;
+                if(!num5.contains(n) && !excludeNum5.contains(n)){
+                    num5.add(n);
+                }
+            }
+
+            //选2个
+            while (true){
+                if(num2.size() == 2){
+                    break;
+                }
+                int m = random.nextInt(10)+1;
+                if(!num2.contains(m) && !excludeNum2.contains(m)){
+                    num2.add(m);
+                }
+            }
+            Collections.sort(num5);
+            Collections.sort(num2);
+            System.out.println(num5.toString()+"  "+num2.toString());
+        }
+    }
+}
